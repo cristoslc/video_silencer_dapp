6f3b20a11e9b88f0f72f9eac80a78dc65deaccaa
Cristos <cristos@cristoslc.com>
2024-03-17 02:57:22 +0000

    fixed app not autostarting in devcontainer

--- Dockerfile
+++ Dockerfile
@@ -11,4 +11,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY ./app /app
 
-CMD ["python", "app.py"]
+CMD ["python", "/app/app.py"]
--- app/app.py
+++ app/app.py
@@ -42,4 +42,4 @@ def download_file(filename):
     return send_from_directory(app.config['PROCESSED_FOLDER'], filename)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
